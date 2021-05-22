681a28756549b3fd9bd76819ab0ee5bca709121b
Huynh Dong <d01295306466@gmail.com>
2021-05-22 21:55:06 +0700

    coding dashboard admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\User;
+use App\Admin;
+use App\Course;
+use App\Lesson;
+use App\ClassRoom;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $users = User::all();
+
+        $courses = Course::all();
+
+        $classes = ClassRoom::all();
+
+        $lessons = Lesson::all();
+
+        $admins = Admin::where('role','Admin')->get();
+
+        $managers = Admin::where('role','Manager')->get();
+
+        return view('admin.home',compact('users','courses','classes','lessons','admins','managers'));
     }
 }
